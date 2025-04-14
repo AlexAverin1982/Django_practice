200d11c236dcaabe382961a5e57e2c736d1cd9e2
Alexander Averin <alexaverin201156@gmail.com>
2025-04-14 14:50:29 +0300

    alpha rollout

--- catalog/urls.py
+++ catalog/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name='home'),
+    path("/", views.home, name='home'),
+    path("contacts/", views.contacts, name='contacts'),
+    path("contacts/posted_info", views.posted_info, name='posted_info'),
 ]
