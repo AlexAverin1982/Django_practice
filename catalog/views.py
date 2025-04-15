5a54d5564fce5e164a293750531af1ce959f1023
Alexander Averin <alexaverin201156@gmail.com>
2025-04-15 14:21:55 +0300

    restructured

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        return render(request, 'response.html')
+    else:
+        return render(request, 'contacts.html')
+
+
+def posted_info(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return render(request, 'response.html', context={'user_name': user_name,
+                                                         'phone': phone, 'message': message})
+    else:
+        return render(request, 'contacts.html')
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from  dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)^=)ty5!b=bcup@%t*+8s%m0q1=517)t!bdf1#sw7y8=snn-2$"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalog"
 ]
 
 MIDDLEWARE = [
@@ -73,13 +76,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+dbname = os.getenv("DBNAME")
+dbuser = os.getenv("USER")
+dbuserpass = os.getenv("PASSWORD")
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': dbname,
+        'USER': dbuser,
+        'PASSWORD': dbuserpass,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
