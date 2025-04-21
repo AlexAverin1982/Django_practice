e73230b46ee64b53fdff6852d10913f57a2857f5
Alexander Averin <alexaverin201156@gmail.com>
2025-04-21 13:49:26 +0300

    custom command add_products

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        cat_veg, _ = Category.objects.get_or_create(name='Овощи свежие')
+
+        vegetables = [
+            {'name': 'Картофель Мордова', 'price': 90.99, 'category': cat_veg},
+            {'name': 'Огурцы грунтовые', 'price': 85.99, 'category': cat_veg},
+            {'name': 'Огурцы гладкие', 'price': 70.99, 'category': cat_veg},
+            {'name': 'Лук репчатый', 'price': 14.99, 'category': cat_veg},
+        ]
+
+        for v_data in vegetables:
+            veg, created = Product.objects.get_or_create(**v_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {veg.name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Product already exists: {veg.name}'))
